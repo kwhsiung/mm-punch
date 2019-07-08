b78b3ec8e01d2d1d6be0d3dbcda9d0c75c5cf452
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-07-08 12:41:54 +0800

    add firebase auth

--- src/punchMembers.js
+++ src/punchMembers.js
@@ -2,8 +2,15 @@ require('dotenv').config()
 const forEach = require('lodash/forEach')
 const firebase = require('firebase')
 firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
   databaseURL: process.env.DATABASE_URL
 })
+firebase
+  .auth()
+  .signInWithEmailAndPassword(
+    process.env.FIREBASE_AUTH_EMAIL,
+    process.env.FIREBASE_AUTH_PASSWORD
+  )
 
 const punch = require('./punch')
 
