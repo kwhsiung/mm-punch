c6092bf0bb040a547a8540ebb1ade51688454b95
Baron Chiu <21715194+bcgodev@users.noreply.github.com>
2021-01-24 10:53:00 +0000

    feat(proxy): disable the use of proxy
    For personal usage ony.

--- tests/proxy.test.js
+++ tests/proxy.test.js
@@ -1,6 +1,6 @@
-const findProxy = require('../src/proxy')
+// const findProxy = require('../src/proxy')
 
-test('could find available proxy', async () => {
-  const proxy = await findProxy()
-  expect(proxy).toMatch(/socks4:\/\//)
-}, 100000)
+// test('could find available proxy', async () => {
+//   const proxy = await findProxy()
+//   expect(proxy).toMatch(/socks4:\/\//)
+// }, 100000)
